90580c77fee8f502a92d6b4b18cdd4f7a5dce4c9
Gabriel D. Padua <gabriel.padua@eduzz.com>
2021-10-28 01:17:18 -0300

    ✨feat(project): courses entity

---
+++ src/courses/courses.entity.ts
@@ -0,0 +1,35 @@
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+@Unique(['name'])
+export class Courses extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ nullable: false, type: 'varchar', length: 250 })
+  name: string;
+
+  @Column({ nullable: false, type: 'varchar', length: 300 })
+  description: string;
+
+  @Column({ type: 'integer' })
+  workload: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}

87c1f3dc11494268af48910c2bf1b5abaf02d5f0
Gabriel D. Padua <gabriel.padua@eduzz.com>
2021-10-29 00:23:55 -0300

    ✨feat(file): courses get and put routes

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { Courses } from './courses.entity';
 import { CoursesService } from './courses.service';
 import { CreateCoursesDto } from './dtos/create-courses.dto';
 import { ReturnCoursesDto } from './dtos/return-courses.dto';
@@ -16,5 +17,32 @@ export class CoursesController {
       course,
       message: 'Success',
     };
+  }
+
+  @Put('/:id')
+  async update(
+    @Param() id: string,
+    @Body() createCourseDto: CreateCoursesDto,
+  ): Promise<ReturnCoursesDto> {
+    const course = await this.coursesService.update(id, createCourseDto);
+    return {
+      course,
+      message: 'Success',
+    };
+  }
+
+  @Get('/:id')
+  async get(@Param() id: string): Promise<ReturnCoursesDto> {
+    const course = await this.coursesService.findOne(id);
+    return {
+      course,
+      message: 'Success',
+    };
+  }
+
+  @Get()
+  async getAll(): Promise<Courses[]> {
+    const courses = await this.coursesService.findAll();
+    return courses;
   }
 }
